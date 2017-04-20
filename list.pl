292ae1e68a2f0646d113bad6ea783de72b80240c
udion <uddeshya.upa@gmail.com>
2017-04-20 18:44:55 +0530

    list Kth element and length added

---
+++ list.pl
@@ -0,0 +1,30 @@
+/**
+This file contains definition of some list predicates which are very handy
+The definitions are based on usually recurssion.
+**/
+
+%predicate to return last element
+my_last(X,[H]):-
+    X=H.
+my_last(X,[_|T]):-
+    my_last(X,T).
+
+%predicate to return second-last element
+my_last_but_1(X,[A,_]):-
+    X=A.
+my_last_but_1(X,[_|T]):-
+    my_last_but_1(X,T).
+
+%predicate to return Kth element
+my_kth(X,[H|_],1):-
+    X=H.
+my_kth(X,[_|T],K):-
+    K_ is K-1,
+    my_kth(X,T,K_).
+
+%predicate to return length of lists
+my_length([_],L):-
+    L=1.
+my_length([_|T],L):-
+    my_length(T,L_),
+    L is L_+1.
